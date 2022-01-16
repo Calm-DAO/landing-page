d8259a08a1efabff82af09f38976c14fc799f72c
sonjoybarmon <sonjoybarmon19@gmail.com>
2022-01-16 13:33:33 +0600

    project setup is done

---
+++ pre-landing-page/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora facere
+        nihil distinctio laudantium ratione incidunt ea rem veniam sequi
+        recusandae?
+      </p>
+    </div>
+  );
+}
+
+export default App;
